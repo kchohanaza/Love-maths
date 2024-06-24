30b2d09bf622e35481f22d09c751956f9c1821b4
Khadeeja Chohan <kchohanaza@gmail.com>
2024-06-24 12:15:10 +0000

    added new structure for JS

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+function calculateCorrectanswer () {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+
+function displaySubtractionQuestion() {
+}
+
+
+function displaymultiplyQuestion() {
+}
